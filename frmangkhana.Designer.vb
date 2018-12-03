3d81e2d03f79a1d6e1bda427cc8b95ca9c7a3793
anongnad <taranongnad@gmail.com>
2018-12-03 20:00:13 +0700

    frmlab2

---
+++ frmangkhana.Designer.vb
@@ -0,0 +1,279 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmangkhana
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtsum = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtsum1 = New System.Windows.Forms.TextBox()
+        Me.lblResult5 = New System.Windows.Forms.Button()
+        Me.lblResult6 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.txtcom = New System.Windows.Forms.Label()
+        Me.txtbox = New System.Windows.Forms.Label()
+        Me.txtcom1 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtsum
+        '
+        Me.txtsum.Font = New System.Drawing.Font("Angsana New", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtsum.Location = New System.Drawing.Point(265, 75)
+        Me.txtsum.Name = "txtsum"
+        Me.txtsum.Size = New System.Drawing.Size(152, 34)
+        Me.txtsum.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Gold
+        Me.Label1.Font = New System.Drawing.Font("Angsana New", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Crimson
+        Me.Label1.Location = New System.Drawing.Point(33, 75)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(142, 29)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "ยอดขาย Vistra Gluta "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.GreenYellow
+        Me.Label3.Font = New System.Drawing.Font("Angsana New", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Blue
+        Me.Label3.Location = New System.Drawing.Point(33, 145)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(136, 29)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "ยอดขาย Frista Glota"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Angsana New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Blue
+        Me.Button1.Location = New System.Drawing.Point(375, 420)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(71, 37)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "คำนวณ"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Angsana New", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Red
+        Me.Button2.Location = New System.Drawing.Point(512, 420)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(65, 37)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "ปิด"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Angsana New", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(261, 20)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(386, 33)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "โปรแกรมยอดขาย Vistra Gluta และยอดขาย Frista Glota"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Angsana New", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(33, 312)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(85, 29)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "ยอดขายรวม"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Angsana New", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(33, 362)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(113, 29)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "ค่าคอมมิชชันรวม"
+        '
+        'txtsum1
+        '
+        Me.txtsum1.Font = New System.Drawing.Font("Angsana New", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtsum1.Location = New System.Drawing.Point(265, 145)
+        Me.txtsum1.Name = "txtsum1"
+        Me.txtsum1.Size = New System.Drawing.Size(152, 34)
+        Me.txtsum1.TabIndex = 0
+        '
+        'lblResult5
+        '
+        Me.lblResult5.BackColor = System.Drawing.Color.LemonChiffon
+        Me.lblResult5.Font = New System.Drawing.Font("Angsana New", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult5.Location = New System.Drawing.Point(375, 312)
+        Me.lblResult5.Name = "lblResult5"
+        Me.lblResult5.Size = New System.Drawing.Size(202, 28)
+        Me.lblResult5.TabIndex = 12
+        Me.lblResult5.UseVisualStyleBackColor = False
+        '
+        'lblResult6
+        '
+        Me.lblResult6.BackColor = System.Drawing.Color.LemonChiffon
+        Me.lblResult6.Font = New System.Drawing.Font("Angsana New", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult6.Location = New System.Drawing.Point(375, 362)
+        Me.lblResult6.Name = "lblResult6"
+        Me.lblResult6.Size = New System.Drawing.Size(202, 28)
+        Me.lblResult6.TabIndex = 12
+        Me.lblResult6.UseVisualStyleBackColor = False
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.BackColor = System.Drawing.Color.Gold
+        Me.Label8.Font = New System.Drawing.Font("Angsana New", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.ForeColor = System.Drawing.Color.Crimson
+        Me.Label8.Location = New System.Drawing.Point(490, 76)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(0, 29)
+        Me.Label8.TabIndex = 13
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.BackColor = System.Drawing.Color.Gold
+        Me.Label9.Font = New System.Drawing.Font("Angsana New", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.ForeColor = System.Drawing.Color.Crimson
+        Me.Label9.Location = New System.Drawing.Point(490, 128)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(0, 29)
+        Me.Label9.TabIndex = 14
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(286, 271)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(0, 13)
+        Me.Label10.TabIndex = 15
+        '
+        'txtcom
+        '
+        Me.txtcom.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.txtcom.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.txtcom.Location = New System.Drawing.Point(267, 205)
+        Me.txtcom.Name = "txtcom"
+        Me.txtcom.Size = New System.Drawing.Size(150, 34)
+        Me.txtcom.TabIndex = 16
+        '
+        'txtbox
+        '
+        Me.txtbox.AutoSize = True
+        Me.txtbox.BackColor = System.Drawing.Color.PowderBlue
+        Me.txtbox.Font = New System.Drawing.Font("Angsana New", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtbox.ForeColor = System.Drawing.Color.Black
+        Me.txtbox.Location = New System.Drawing.Point(33, 213)
+        Me.txtbox.Name = "txtbox"
+        Me.txtbox.Size = New System.Drawing.Size(131, 26)
+        Me.txtbox.TabIndex = 17
+        Me.txtbox.Text = "ยอดค่าคอมมิชชั่น Vistra"
+        '
+        'txtcom1
+        '
+        Me.txtcom1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.txtcom1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.txtcom1.Location = New System.Drawing.Point(267, 260)
+        Me.txtcom1.Name = "txtcom1"
+        Me.txtcom1.Size = New System.Drawing.Size(150, 34)
+        Me.txtcom1.TabIndex = 16
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.BackColor = System.Drawing.Color.PowderBlue
+        Me.Label11.Font = New System.Drawing.Font("Angsana New", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label11.ForeColor = System.Drawing.Color.Black
+        Me.Label11.Location = New System.Drawing.Point(33, 268)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(129, 26)
+        Me.Label11.TabIndex = 17
+        Me.Label11.Text = "ยอดค่าคอมมิชชั่น Frista"
+        '
+        'frmangkhana
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Pink
+        Me.ClientSize = New System.Drawing.Size(861, 481)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.txtbox)
+        Me.Controls.Add(Me.txtcom1)
+        Me.Controls.Add(Me.txtcom)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.lblResult6)
+        Me.Controls.Add(Me.lblResult5)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtsum1)
+        Me.Controls.Add(Me.txtsum)
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Name = "frmangkhana"
+        Me.TransparencyKey = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtsum As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents txtsum1 As System.Windows.Forms.TextBox
+    Friend WithEvents lblResult5 As System.Windows.Forms.Button
+    Friend WithEvents lblResult6 As System.Windows.Forms.Button
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents txtcom As System.Windows.Forms.Label
+    Friend WithEvents txtbox As System.Windows.Forms.Label
+    Friend WithEvents txtcom1 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+
+End Class
---
+++ frmangkhana.vb
@@ -0,0 +1,62 @@
+﻿Public Class frmangkhana
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtsum.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblResult5.Click
+
+    End Sub
+
+    Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        
+        Dim Vistra, Frista, sell As Integer
+        Dim VistraCom, FristaCom, TotalCommition As Long
+        Vistra = txtsum.Text
+        Frista = txtsum1.Text
+
+        sell = Vistra + Frista
+
+        VistraCom = Vistra * 5 / 100
+        FristaCom = Frista * 10 / 100
+
+        txtcom.Text = VistraCom
+        txtcom1.Text = FristaCom
+
+        TotalCommition = VistraCom + FristaCom
+        
+        lblResult5.Text = sell
+        lblResult6.Text = TotalCommition
+
+
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtsum1.TextChanged
+
+    End Sub
+
+    Private Sub txtsum_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtcom.Click, txtcom1.Click
+
+    End Sub
+
+    Private Sub Label4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtbox.Click, Label11.Click
+
+    End Sub
+End Class
